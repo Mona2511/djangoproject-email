c451dbc2f4006e89461f0213061afa96dec106c8
Monali Changla <changlamonali.24.cse@iite.indusuni.ac.in>
2026-06-18 13:12:00 +0530

    Add files via upload

---
+++ myuserapp/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views 
+
+urlpatterns = [
+    path('', views.homepageview),
+    path('home',views.homepageview),
+    path('about',views.aboutpageview),
+    path('contact',views.contactpageview),
+    path('contactprocess',views.contactprocess),
+    path('shop',views.shoppage),
+    path('saveSession',views.saveSessionData),
+    path('getSession',views.getSessionData),
+    path('getSession2',views.getSessionData2),
+    path('removeSession',views.deleteSessionData),
+    path('login',views.loginpage),
+    path('loginprocess',views.loginprocess),
+    path('dashboard',views.dashboard),
+    path('logout',views.logout)
+]
